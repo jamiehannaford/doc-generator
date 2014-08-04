1dade6f876693dfb76205efbfd13ce2065747f69
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-08-04 16:49:53 +0200

    Modifying parameter table generator

--- spec/OpenStack/DocGenerator/ParameterTableGeneratorSpec.php
+++ spec/OpenStack/DocGenerator/ParameterTableGeneratorSpec.php
@@ -23,15 +23,6 @@ class ParameterTableGeneratorSpec extends ObjectBehavior
         $this->beConstructedWith($operation, $stream);
     }
 
-    public function getMatchers()
-    {
-        return [
-            'containString' => function(StreamInterface $stream, $string) {
-                return $string === \GuzzleHttp\Stream\copy_to_string($stream);
-            }
-        ];
-    }
-
     function it_is_initializable()
     {
         $this->shouldHaveType('OpenStack\DocGenerator\ParameterTableGenerator');
@@ -42,65 +33,29 @@ class ParameterTableGeneratorSpec extends ObjectBehavior
         $this->getStream()->shouldReturnAnInstanceOf('GuzzleHttp\Stream\StreamInterface');
     }
 
-    function it_should_write_section_header_to_stream()
+    public function it_should_write_table()
     {
-        $string = <<<EOT
-Parameters
-~~~~~~~~~~
-EOT;
-
-        $this->stream->write($string)->shouldBeCalled();
-
-        $this->writeSectionHeader();
-    }
-
-    function it_should_write_directive_to_stream()
-    {
-        $string = ".. csv-table::\n";
-
-        $this->stream->write($string)->shouldBeCalled();
-
-        $this->writeDirective();
-    }
-
-    function it_should_write_titles_to_stream()
-    {
-        $string = ':header: "Name", "Type", "Required", "Description"' . "\n";
-
-        $this->stream->write($string)->shouldBeCalled();
-
-        $this->writeTitles();
-    }
-
-    function it_should_write_widths_to_stream()
-    {
-        $string = ":widths: 20, 20, 10, 50\n";
-
-        $this->stream->write($string)->shouldBeCalled();
-
-        $this->writeWidths();
-    }
-
-    function it_should_call_parameter_for_required_values()
-    {
-        $this->parameter->getName()->shouldBeCalled();
-        $this->parameter->getType()->shouldBeCalled();
-        $this->parameter->getRequired()->shouldBeCalled();
-        $this->parameter->getDescription()->shouldBeCalled();
-
-        $this->writeParameters();
-    }
-
-    function it_should_write_param_values_into_csv_format()
-    {
+        $this->parameter->getEnum()->shouldBeCalled();
         $this->parameter->getName()->willReturn('a');
         $this->parameter->getType()->willReturn('b');
         $this->parameter->getRequired()->willReturn(false);
         $this->parameter->getDescription()->willReturn('d');
 
-        $string = '"a","b","No","d"' . "\n";
-        $this->stream->write($string)->shouldBeCalled();
+        $block = <<<'EOT'
+Parameters
+~~~~~~~~~~
 
-        $this->writeParameters();
+.. csv-table::
+
+  :header: "Name", "Type", "Required", "Description"
+  :widths: 20, 20, 10, 50
+
+  "a", "b", "No", "d"
+
+EOT;
+
+        $this->stream->write($block)->shouldBeCalled();
+
+        $this->writeAll();
     }
 }
--- src/OpenStack/DocGenerator/ParameterTableGenerator.php
+++ src/OpenStack/DocGenerator/ParameterTableGenerator.php
@@ -2,9 +2,6 @@
 
 namespace OpenStack\DocGenerator;
 
-use GuzzleHttp\Command\Guzzle\Operation;
-use GuzzleHttp\Stream\StreamInterface;
-
 /**
  * Generates a representation of {@see \GuzzleHttp\Command\Guzzle\Parameter}s
  * in order for them to be read by end-users in documentation. The generated
@@ -14,74 +11,26 @@ use GuzzleHttp\Stream\StreamInterface;
  * @package OpenStack\DocGenerator
  * @link http://docutils.sourceforge.net/docs/ref/rst/directives.html#csv-table
  */
-class ParameterTableGenerator
+class ParameterTableGenerator extends AbstractGenerator
 {
-    /**
-     * @var \GuzzleHttp\Command\Guzzle\Parameter[]
-     */
-    private $parameters;
-
-    /**
-     * @var StreamInterface
-     */
-    private $stream;
-
-    /**
-     * @param Operation       $operation
-     * @param StreamInterface $stream
-     */
-    public function __construct(Operation $operation, StreamInterface $stream)
-    {
-        $this->parameters = $operation->getParams();
-        $this->stream     = $stream;
-    }
-
-    /**
-     * @return StreamInterface
-     */
-    public function getStream()
-    {
-        return $this->stream;
-    }
-
-    /**
-     * Writes section header
-     */
-    public function writeSectionHeader()
-    {
-        $string = 'Parameters'  . PHP_EOL . '~~~~~~~~~~' . PHP_EOL;
-
-        $this->output($string);
-    }
-
-    /**
-     * Writes the Sphinx directive
-     */
-    public function writeDirective()
-    {
-        $string = '.. csv-table::';
-
-        $this->output($string);
-    }
-
     /**
      * Writes the titles of the columns
      */
-    public function writeTitles()
+    private function writeTitles()
     {
         $string = ':header: "Name", "Type", "Required", "Description"';
 
-        $this->output($string, true);
+        $this->buffer($string, true);
     }
 
     /**
      * Writes the widths of the columns
      */
-    public function writeWidths()
+    private function writeWidths()
     {
         $string = ':widths: 20, 20, 10, 50' . PHP_EOL;
 
-        $this->output($string, true);
+        $this->buffer($string, true);
     }
 
     /**
@@ -89,7 +38,7 @@ class ParameterTableGenerator
      * pertinent information - these are its name, type, whether its required
      * or not, and its description.
      */
-    public function writeParameters()
+    private function writeParameters()
     {
         foreach ($this->parameters as $param) {
 
@@ -113,17 +62,8 @@ class ParameterTableGenerator
                 $param->getDescription()
             );
 
-            $this->output($string, true);
+            $this->buffer($string, true);
         }
-    }
-
-    private function output($string, $indent = false)
-    {
-        if ($indent) {
-            $string = '  ' . $string;
-        }
-
-        $this->stream->write($string . PHP_EOL);
     }
 
     /**
@@ -131,10 +71,12 @@ class ParameterTableGenerator
      */
     public function writeAll()
     {
-        $this->writeSectionHeader();
-        $this->writeDirective();
+        $this->writeSectionHeader('Parameters');
+        $this->writeDirective('csv-table');
         $this->writeTitles();
         $this->writeWidths();
         $this->writeParameters();
+
+        $this->write();
     }
 }
